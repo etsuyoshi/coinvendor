079e60dc02850c3bd505f6c3175fa57ba5ce7f65
tuyo1009 <tuyo1009@gmail.com>
2015-03-10 18:41:30 +0900

    Initial Commit

---
+++ coinvendor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  coinvendor
+//
+//  Created by EndoTsuyoshi on 2015/03/10.
+//  Copyright (c) 2015年 com.endo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
